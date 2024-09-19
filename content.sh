a2cfdeba0c5b368934efeb6b36eb7e4270c821f0
Marko Doda <markododa@protonmail.com>
2024-09-19 21:51:17 +0200

    script to record clicks

--- content.sh
+++ content.sh
@@ -3,5 +3,5 @@ while read ime broj _; do
 if [ $ime == "ime" ]; then
 	continue
 fi
-sed -i "/header.png/a \<div class="button"><a href=\"tel://$broj\">$ime</a></div>" $2
+sed -i "/header.png/a \<div class="button"><a onclick="post\("$ime"\)" id="$ime" href=\"tel://$broj\">$ime</a></div>" $2
 done <<< $(tac $1)
