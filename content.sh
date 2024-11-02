fdfd9c1bdac5250eed2d83c40b234d65d114a90a
Marko Doda <markododa@protonmail.com>
2024-11-02 12:00:12 +0100

    Add wizi, rework bash script

--- content.sh
+++ content.sh
@@ -1,7 +1,9 @@
 export IFS=,
+cp index.tmpl public/index.html
 while read ime broj _; do
 if [ $ime == "ime" ]; then
 	continue
 fi
-sed -i "/header.png/a \<div class="button"><a onclick="post\(\"$ime\"\)" id="$ime" href=\"tel://$broj\">$ime</a></div>" $2
-done <<< $(tac $1)
+
+sed -i "/header.png/a \<div class="button"><a onclick="post\(\"$ime\"\)" id="$ime" href=\"tel://$broj\">$ime</a></div>" public/index.html
+done <<< $(tac taxi.csv)
